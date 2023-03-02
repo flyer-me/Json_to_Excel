0c4b3be29e8afbd8821ab60d8a49c7df63c32338
Admin <61903267+flyer-me@users.noreply.github.com>
2023-03-02 16:33:21 +0800

    release

--- auto_set.py
+++ auto_set.py
@@ -5,6 +5,7 @@ import pandas as pd
 import json
 import os
 import chardet
+import csv
 
 # 定义json_to_excel函数 此处处理为：json内容位于每行的最后，将其向右展开并添加json对应键的表头 按需修改
 def json_to_excel(wb):
@@ -24,13 +25,15 @@ def json_to_excel(wb):
             continue
         # 获取单元格内容
         value = cell.value
-        print(value)
+        if value == None:
+            continue
         # 匹配json字符串
         match = pattern.search(value)
-        print(match)
         # 如果匹配成功，提取json字符串，并转换为字典
         if match:
             json_str = match.group()
+            
+            json_str = json_str.replace('_x000D_','\r')
             json_dict = eval(json_str)
 
             # 根据字典生成新表格
@@ -54,7 +57,13 @@ csv_files = [f for f in os.listdir('.') if f.endswith('.csv')]
 for file in csv_files:
     encoding = chardet.detect(open(file, 'rb').read())['encoding']
     print("文件编码：",encoding)
-    df = pd.read_csv(file,sep=',',encoding=encoding)
+    
+    #   针对以=不同分隔符的读取
+    try:
+        df = pd.read_csv(file, encoding=encoding, sep=',')
+    except:
+        df = pd.read_csv(file, encoding=encoding, sep='\t')
+
     xlsx_name = file.replace('.csv', '.xlsx')
     df.to_excel(xlsx_name, index=False)
 
